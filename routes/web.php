7cf57a95e28cb60a64b11e05437419b53212693e
Echezona-Mbah <echezonaernest6@gmaill.com>
2023-11-09 07:29:28 -0800

    Newer

--- routes/web.php
+++ routes/web.php
@@ -45,10 +45,4 @@ Route::get('/terms', function () {
     return view('terms');
 });
 
-Route::get('/blog-detail', function () {
-    return view('blog-detail');
-});
 
-Route::get('/blog-detail2', function () {
-    return view('blog-detail2');
-});
